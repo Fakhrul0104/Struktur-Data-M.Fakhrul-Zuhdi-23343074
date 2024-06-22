203fb5b5efef863a8471e563c62b52a501655159
Fakhrul0104 <144107616+Fakhrul0104@users.noreply.github.com>
2024-06-22 22:50:16 +0700

    Delete JobSheet 1 directory

--- JobSheet 1/Program/Latihan/L1 JS1.cpp
+++
@@ -1,6 +0,0 @@
-//Created by_M.Fakhrul Zuhdi 23343074
-#include <stdio.h>
-int main(){
-	printf("\nWelcome to the world of C");//print the mesege on the screen
-	return 0;//return a value 0 to the operating system
-}
